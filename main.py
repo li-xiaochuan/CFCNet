bad59f7daba3c098e063e1bac68e65071553fa5c
li-xiaochuan <2320400037@stu.hrbust.edu.cn>
2026-03-06 13:48:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+import os
+import torch
+import argparse
+from torch.backends import cudnn
+from models.CFCNet import build_net
+from train import _train
+from eval import _eval
+
+def main(args):
+    # CUDNN
+    cudnn.benchmark = True
+
+    if not os.path.exists('results/'):
+        os.makedirs(args.model_save_dir)
+    if not os.path.exists('results/' + args.model_name + '/'):
+        os.makedirs('results/' + args.model_name + '/')
+    if not os.path.exists(args.result_dir):
+        os.makedirs(args.result_dir)
+    model = build_net()
+    # print(model)
+
+    if torch.cuda.is_available():
+        model.cuda()
+    if args.mode == 'train':
+        _train(model, args)
+
+    elif args.mode == 'test':
+        _eval(model, args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Directories
+    parser.add_argument('--model_name', default='CFCNet', type=str)
+    parser.add_argument('--data', type=str, default='Haze4K', choices=['ITS', 'Haze4K', 'NHR', 'GTA5', 'real_haze'])
+
+    parser.add_argument('--mode', default='train', choices=['train', 'test'], type=str)
+    parser.add_argument('--data_dir', type=str, default='your_path/Haze4K')
+
+    # Train for its
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--learning_rate', type=float, default=4e-4)
+    parser.add_argument('--weight_decay', type=float, default=0)
+    parser.add_argument('--num_epoch', type=int, default=1000)
+    parser.add_argument('--print_freq', type=int, default=100)
+    parser.add_argument('--num_worker', type=int, default=8)
+    parser.add_argument('--save_freq', type=int, default=20)
+    parser.add_argument('--valid_freq', type=int, default=20)
+    parser.add_argument('--resume', type=str, default='')
+
+
+    # uncomment for different datasets
+
+    # Train for real-haze
+    # parser.add_argument('--batch_size', type=int, default=2)
+    # parser.add_argument('--learning_rate', type=float, default=2e-4)
+    # parser.add_argument('--weight_decay', type=float, default=0)
+    # parser.add_argument('--num_epoch', type=int, default=5000)
+    # parser.add_argument('--print_freq', type=int, default=20)
+    # parser.add_argument('--num_worker', type=int, default=4)
+    # parser.add_argument('--save_freq', type=int, default=10)
+    # parser.add_argument('--valid_freq', type=int, default=10)
+
+    # Train for Haze4k
+    # parser.add_argument('--batch_size', type=int, default=8)
+    # parser.add_argument('--learning_rate', type=float, default=4e-4)
+    # parser.add_argument('--weight_decay', type=float, default=0)
+    # parser.add_argument('--num_epoch', type=int, default=1000)
+    # parser.add_argument('--print_freq', type=int, default=100)
+    # parser.add_argument('--num_worker', type=int, default=8)
+    # parser.add_argument('--save_freq', type=int, default=20)
+    # parser.add_argument('--valid_freq', type=int, default=20)
+
+    # Test
+    parser.add_argument('--test_model', type=str, default='Best-4k.pkl')
+    parser.add_argument('--save_image', type=bool, default=False, choices=[True, False])
+
+    args = parser.parse_args()
+    args.model_save_dir = os.path.join('results/', args.model_name, 'Training-Results/')
+    args.result_dir = os.path.join('results/', args.model_name, 'images', args.data)
+    if not os.path.exists(args.model_save_dir):
+        os.makedirs(args.model_save_dir)
+    command = 'cp ' + 'models/layers.py ' + args.model_save_dir
+    os.system(command)
+    command = 'cp ' + 'models/CFCNet.py ' + args.model_save_dir
+    os.system(command)
+    command = 'cp ' + 'train.py ' + args.model_save_dir
+    os.system(command)
+    command = 'cp ' + 'main.py ' + args.model_save_dir
+    os.system(command)
+    print(args)
+    main(args)
